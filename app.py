db8844bcc99959afda53b25762eb7b5c57075971
MClode <MClode@users.noreply.github.com>
2017-06-03 21:04:39 +0100

    O(n) implementation of the same problem, after some thinking! Shorter, simpler, better.

--- app.py
+++ app.py
@@ -2,46 +2,21 @@ class StockCalculator:
 
     def __init__(self, list):
         if len(list) < 2:
-            raise Exception("List needs more than one item!")
+            raise Exception("The list needs more than one item!")
         self.list = list
 
-    def check_for_loss(self):
-        # Special case is when the list is a reverse sorted list (i.e. you are guaranteed to lose money)
-
-        loss = self.list[1] - self.list[0]
-
-        for key, val in enumerate(self.list):
-            if key == 0:
-                continue
-            previous_val = self.list[key-1]
-            if previous_val >= val:
-                if (val - previous_val) > loss:
-                    loss = val - previous_val
-            else:
-                return 1
-
-        return loss
-
-    # returns 6 (buying for $5 and selling for $11)
-    def get_max_profit(self):
-
-        sell, profit = 0, 0
-
-        for i in self.list:
-            buy = self.list.pop(0)
-            for x in self.list:
-                if x < buy or x < sell:
-                    continue
-
-                sell = x
-                profit = x - buy
-
-        return profit
-
     def find(self):
-        loss = self.check_for_loss()
-        if loss != 1:
-            return loss
-        else:
-            return self.get_max_profit()
 
+        min_price = self.list[0]
+        max_profit = self.list[1] - self.list[0]
+
+        # We need to iterate over the list just once to get O(n). We will always know our highest profit by simply
+        # keeping track of profit and the lowest previous price. If the current price gives us a better profit, we
+        # update max profit. A simple greedy algorithm
+        for price in self.list[1:]:
+            if (price - min_price) > max_profit:
+                max_profit = price - min_price
+
+            min_price = min(price, min_price)
+
+        return max_profit
