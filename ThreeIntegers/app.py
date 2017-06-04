f3cf5f02a84c0b09fec340acafa9424c50929f79
MClode <MClode@users.noreply.github.com>
2017-06-04 20:37:22 +0100

    O(n) time and O(1) space solution

--- ThreeIntegers/app.py
+++ ThreeIntegers/app.py
@@ -9,9 +9,41 @@ class ThreeIntegers:
     def __init__(self, list):
         if len(list) < 3:
             raise Exception("The list needs at least three numbers!")
+        self.positive_integers = [0, 0, 0]
+        self.negative_integers = [0, 0]
         self.list = list
 
     def find(self):
-        max_product = 1
+        for number in self.list:
+            if number > 0:
+                self.positive_numbers(number, self.positive_integers)
+            if number < 0:
+                self.negative_numbers(number, self.negative_integers)
 
-        return max_product
+        return max(
+            self.positive_integers[0]*self.positive_integers[1]*self.positive_integers[2],
+            self.negative_integers[0]*self.negative_integers[1]*self.positive_integers[0],
+        )
+
+    def positive_numbers(self, n, list):
+        if n > list[0]:
+            list[1] = max(list[0], list[1])
+            list[2] = max(list[1], list[2])
+            list[0] = n
+            return
+        if n > list[1]:
+            list[2] = max(list[1], list[2])
+            list[1] = n
+            return
+        if n > list[2]:
+            list[2] = n
+            return
+
+    def negative_numbers(self, n, list):
+        if n < list[0]:
+            list[1] = min(list[0], list[1])
+            list[0] = n
+            return
+        if n < list[1]:
+            list[1] = n
+            return
--- ThreeIntegers/calc_test.py
+++ ThreeIntegers/calc_test.py
@@ -9,6 +9,10 @@ class ThreeIntegersTest(unittest.TestCase):
 
         self.assertEqual(Calc.find(), 10*8*7)
 
+    def test_negative_numbers(self):
+        Calc = ThreeIntegers([-10, -10, 1, 3, 2])
+
+        self.assertEqual(Calc.find(), -10*-10*3)
 
 if __name__ == '__main__':
     unittest.main()
