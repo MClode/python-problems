915ff96512231b3463fe6cda76333fccf2e9a6ba
MClode <martin@workdigital.co.uk>
2017-06-18 21:08:16 +0000

    O(n) time and O(1) space, using the Knuth Shuffle.

--- ShuffleArray/app.py
+++ ShuffleArray/app.py
@@ -1,22 +1,29 @@
-from random import randint
+"""
+Write a function for doing an in-place ↴ shuffle of a list.
+The shuffle must be "uniform," meaning each item in the original list must have the same probability of ending up in each spot in the final list.
 
+Assume that you have a function get_random(floor, ceiling) for getting a random integer that is >= floor and <= ceiling.
+"""
+
+from random import randint
 
 class ShuffleArray:
 
-	def get_random(self, floor, ceiling):
-		return randint(floor, ceiling)
-	
-	def run(self, array):
-		new_array = array[:]
-		
-		i = 0
-		while i < len(new_array):
-			key_to_pop = self.get_random(0, len(array)-1)
-			popped = array.pop(key_to_pop)
-			
-			new_array[i] = popped
-			
-			i += 1
-			
-		return new_array
-		
+  def __init__(self, l):
+    self.l = l
+
+  def get_random(self, floor, ceiling):
+    return randint(floor, ceiling)
+
+  def run(self):
+
+    i = 0
+    while i < len(self.l):
+      key = self.get_random(i, len(self.l)-1)
+
+      self.l[i], self.l[key] = self.l[key], self.l[i]
+
+      i += 1
+
+    return self.l
+
--- ShuffleArray/shuffle_test.py
+++ ShuffleArray/shuffle_test.py
@@ -4,13 +4,20 @@ from ShuffleArray.app import ShuffleArray
 
 
 class ShuffleArrayTest(unittest.TestCase):
-    def test_array_same_size(self):
-        shuffle = ShuffleArray()
-        input = [1, 7, 3, 4]
-        res = shuffle.run(input[:])
 
-        self.assertEqual(len(input), len(res))
+  def setUp(self):
+    self.original = [1, 2, 3, 4]
+    self.shuffled = ShuffleArray(self.original[:]).run()
+
+  def test_array_same_size(self):
+    self.assertEqual(len(self.original), len(self.shuffled))
+
+  def test_arrays_have_equivalent_content(self):
+    """ Despite being terribly named, assertCountEqual actually does exactly what I need.
+        It will assert that the items are equivalent, disregarding order. """
+    self.assertCountEqual(self.original, self.shuffled)
 
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
+
